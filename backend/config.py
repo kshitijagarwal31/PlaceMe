c31e822c8e9892d5e3e7ba3113b5393e92b350f6
kshitijagarwal31 <kshitijagarwal999@gmail.com>
2026-06-07 23:46:42 +0530

    Update config.py

--- backend/config.py
+++ backend/config.py
@@ -1,19 +1,21 @@
+import os
+
 class Config():
     DEBUG = False 
     SQLALCHEMY_TRACK_MODIFICATIONS = True 
     
     
 class LocalDevelopmentConfig(Config):
-    SQLALCHEMY_DATABASE_URI = "postgresql://postgres:admin123@localhost/placement_portal" 
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
     DEBUG = True
     
-    SECRET_KEY = "secret-key-here"
+    SECRET_KEY = os.getenv("SECRET_KEY")
     SECURITY_PASSWORD_HASH = "argon2"
-    SECURITY_PASSWORD_SALT = "password-salt-here"
+    SECURITY_PASSWORD_SALT = os.getenv("SECURITY_PASSWORD_SALT")
     WTF_CSRF_ENABLED = False
     SECURITY_TOKEN_AUTHENTICATION_HEADER = "Authentication-Token"
     
     CACHE_TYPE            = "RedisCache"
-    CACHE_REDIS_URL       = "redis://localhost:6379/0"
+    CACHE_REDIS_URL       = os.getenv("REDIS_URL")
     CACHE_DEFAULT_TIMEOUT = 300
     CACHE_KEY_PREFIX      = ""
