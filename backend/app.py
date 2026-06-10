d5dd3c19cb3f3d991305ca492e4c64a3c727c460
kshitijagarwal31 <kshitijagarwal999@gmail.com>
2026-06-10 13:50:36 +0530

    CORS POST requests fix

--- backend/app.py
+++ backend/app.py
@@ -20,7 +20,11 @@ def create_app():
 
     db.init_app(app)
     cache.init_app(app)
-    CORS(app, origins=["https://place-me-one.vercel.app"], supports_credentials=True)
+    CORS(app, 
+    origins=["https://place-me-one.vercel.app"],
+    supports_credentials=True,
+    methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allow_headers=["Content-Type", "Authorization", "Authentication-Token"])
 
     app.register_blueprint(auth_bp)
     app.register_blueprint(admin_bp)
